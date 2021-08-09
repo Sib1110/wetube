ba1287b4411f197edefd86b6c4ef58b361bda33d
Wonit2 <powhonor@gmail.com>
2021-08-09 23:23:10 +0900

    6.6

--- src/controllers/videoContoroller.js
+++ src/controllers/videoContoroller.js
@@ -27,31 +27,44 @@ let videos = [
 
 export const trending = (req, res) => {
   return res.render("home", {pageTitle: "Home", videos});
-}
+};
 export const watch = (req, res) => {
     const { id } = req.params;
     const video = videos[id-1];
     return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
-}
+};
 export const getEdit = (req, res) => {
     const { id } = req.params;
     const video = videos[id-1];
   return res.render("edit", {pageTitle:`Editing: ${video.title}` , video})
-}
+};
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
-    videos[0].title = title;
+    videos[id-1].title = title;
     return res.redirect(`/videos/${id}`);
-} 
+}; 
+
+export const getUpload = (req, res) => {
+    return res.render("upload",{pageTitle: "Upload video"});
+};
+export const postUpload = (req, res) => {
+    const newVideo = {
+        title: req.body.title,
+        rating: 0,
+        comments: 0,
+        views: 0,
+        id: videos.length + 1,
+    };
+    videos.push(newVideo);
+    return res.redirect("/");
+};
 
 
 
 export const search = (req, res) => res.send("search")
-export const upload = (req, res) => res.send("upload")
 
 export const deleteVideo = (req, res) => {
     console.log(req.params)
     return res.send("Delete Video")
-}
-
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,19 +2,23 @@ import express from 'express';
 import {
     watch, 
     getEdit, 
-    upload, 
+     
     deleteVideo,
     postEdit,
+    getUpload,
+    postUpload,
 } from '../controllers/videoContoroller'
 
 
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
+
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
+videoRouter.get("/upload", getUpload);
+videoRouter.post("/upload", postUpload);
 
 export default videoRouter;
