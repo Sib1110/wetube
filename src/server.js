7fc00b3c012b3268b062e7b1235b492ba01a2689
Wonit2 <powhonor@gmail.com>
2021-08-10 16:18:20 +0900

    6.18

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-import "./db";
+
 import express from 'express';
 import morgan from 'morgan';
 import globalRouter from './routers/globalRouter';
 import videoRouter from './routers/videoRouter';
 import userRouter from './routers/userRouter';
 
-const PORT = 4000;
+
 const app = express();
 const logger = morgan("dev");
 
@@ -18,10 +18,7 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
+export default app;
 
-const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
-
-
-app.listen(PORT, handleListening)
 
 
