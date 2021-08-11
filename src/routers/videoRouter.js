aa5f5e999934bb0de0a46b962fcec9762d95fbf8
Wonit2 <powhonor@gmail.com>
2021-08-11 15:46:34 +0900

    6.28

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,7 +2,6 @@ import express from 'express';
 import {
     watch, 
     getEdit, 
-     
     deleteVideo,
     postEdit,
     getUpload,
@@ -16,8 +15,7 @@ const videoRouter = express.Router();
 
 videoRouter.get(("/:id([0-9a-f]{24})"), watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.get("/upload", getUpload);
 videoRouter.post("/upload", postUpload);
 
