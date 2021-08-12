cc5f8141af0f5f0460629faac245e373c1946c33
Wonit2 <powhonor@gmail.com>
2021-08-12 19:18:40 +0900

    7.11

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import {getJoin, postJoin,login} from '../controllers/userController'
-import {home, search} from '../controllers/videoContoroller';
-
+import express from "express";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controllers/userController";
+import { home, search } from "../controllers/videoContoroller";
 
 const rootRouter = express.Router();
 
-
-
-rootRouter.get("/", home)
+rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login)
-rootRouter.get("/search", search)
-
-
+rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.get("/search", search);
 
 export default rootRouter;
