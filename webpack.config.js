071f8ace4e6aee585326f2ac714ec07f8a906c13
Wonit2 <powhonor@gmail.com>
2021-08-21 20:01:33 +0900

    9.5 connection CSS,SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/client/js/main.js",
   mode: "development",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
   },
   module: {
     rules: [
@@ -17,6 +23,11 @@ module.exports = {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        //loader를 적는 순서는 역순으로 해야함. 왜? webpack 뒤에서부터 순차적으로 실행하기때문에.
       },
     ],
   },
