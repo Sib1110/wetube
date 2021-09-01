1d5fffa48088d218c2041a6d5d7f19c1552457a5
Wonit2 <powhonor@gmail.com>
2021-09-01 13:36:59 +0900

    16.7

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,6 +1,18 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
 const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
@@ -9,7 +21,7 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     //fetch는 url변경없이 request를 보내게해줌.
     method: "POST",
     //header에서 이렇게 지정해줌으로써 백엔드로 텍스트를 보내는것이아니라
@@ -19,7 +31,9 @@ const handleSubmit = async (event) => {
   }); // fetch로 부터 오는 file은 주로(?) JSON형태임
   // 매우 큰 object를 프론트엔드에서 백엔드로 보낼때 쓰는 방법.
   textarea.value = ""; // <-- getter이면서 setter임.
-  window.location.reload();
+  if (status === 201) {
+    addComment(text);
+  }
 };
 
 if (form) {
